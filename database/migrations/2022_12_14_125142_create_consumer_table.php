3c81039ec254ef69a4d059075394b5f13f891438
Humoyunmirzo Yoqubjonov <humoyunmirzoyoqubjonov@gmail.com>
2022-12-14 19:57:33 +0500

    consumer table

--- database/migrations/2022_12_14_125142_create_consumer_table.php
+++ database/migrations/2022_12_14_125142_create_consumer_table.php
@@ -18,6 +18,8 @@ return new class extends Migration {
             $table->string('name');
             $table->string('phone');
 
+            $table->string('manzil');
+
             $table->string('viloyat');
             $table->string('tuman');
 
